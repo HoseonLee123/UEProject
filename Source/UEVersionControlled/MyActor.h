79be961d7958c761d285e5d13b43f1769731ba6e
HoseonLee <hoseon310@gmail.com>
2024-09-27 17:13:09 +0900

    Added New Actor

--- Source/UEVersionControlled/MyActor.h
+++ Source/UEVersionControlled/MyActor.h
@@ -9,18 +9,20 @@
 UCLASS()
 class UEVERSIONCONTROLLED_API AMyActor : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AMyActor();
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    AMyActor();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
+    UPROPERTY(EditAnywhere)
+    class UStaticMeshComponent* MeshComp;
 };
